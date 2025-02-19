f01b739e3ab51b2f16121c5dedf42f13239781fb
Halil-ozcan <halil_ozcann@outlook.com>
2025-02-19 21:51:01 +0300

    HomeWork07 Ödevi Yapıldı

---
+++ 01-C# ile Temel Proglamlama/Hafta-01/HomeWork_18_02_2025/HomeWork07/Program.cs
@@ -0,0 +1,9 @@
+﻿int sayi = 1000000;
+
+byte sayiDonusumu = (byte)sayi;
+
+
+Console.WriteLine($"Int değeri: {sayi}");
+Console.WriteLine($"Byte değeri: {sayiDonusumu}");
+
+// Byte değeri 0-255 arasında değer aldığı için 1000000 değeri byte ın alacağı değerden büyük olduğu için Taşma(Overflow) meydana gelir. Yani Özet olarak 1000000 sayısını 256 bölüp kalanı 64 sayısını bulmuş oluruz ve ekrana 1000000 birinci çıktı 64 ikinci çıktı olarak gösterilir.
