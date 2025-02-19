ded64c734c16bf4a5edd6774d5aabcb68065e08e
Halil-ozcan <halil_ozcann@outlook.com>
2025-02-19 21:58:02 +0300

    HomeWork08 Ödevi Yapıldı.

---
+++ 01-C# ile Temel Proglamlama/Hafta-01/HomeWork_18_02_2025/HomeWork08/Program.cs
@@ -0,0 +1,8 @@
+﻿double sayi1 = 3.14;
+
+int sonuc = (int)sayi1;
+
+Console.WriteLine($"Double değeri: {sayi1}");
+Console.WriteLine($"Int değeri: {sonuc}");
+
+// Double bellekte ondalıklı bir sayı tutar. İnt değişkeni ise tam sayı tutar. bu yüzden sonuc 3 olarak yazılır.
