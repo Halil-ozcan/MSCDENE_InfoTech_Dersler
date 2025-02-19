901b7fb81d5c095271291cfa966ca3696df52822
Halil-ozcan <halil_ozcann@outlook.com>
2025-02-19 22:04:19 +0300

    HomeWork09 Ödevi Yapıldı.

---
+++ 01-C# ile Temel Proglamlama/Hafta-01/HomeWork_18_02_2025/HomeWork09/Program.cs
@@ -0,0 +1,7 @@
+﻿string sayi = "42";
+
+int donusum = int.Parse(sayi);
+int donusum2 = Convert.ToInt32(sayi);
+
+Console.WriteLine($"String değeri: {sayi}");
+Console.WriteLine($"Int değeri: {donusum} {donusum2}");
