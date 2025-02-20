7e5f6d6f07f6f1d77d08c6c845590ba896035c79
Halil-ozcan <halil_ozcann@outlook.com>
2025-02-20 21:24:35 +0300

    HomeWork15 Ödevi Yapıldı

---
+++ 01-C# ile Temel Proglamlama/Hafta-01/HomeWork_18_02_2025/HomeWork15/Program.cs
@@ -0,0 +1,9 @@
+﻿string dogruMu = "True";
+
+bool donusum = bool.Parse(dogruMu);
+
+bool donusum2 = Convert.ToBoolean(dogruMu);
+
+Console.WriteLine($"String değeri: {dogruMu}");
+Console.WriteLine($"Bool değeri1: {donusum}");
+Console.WriteLine($"Bool değeri2: {donusum2}");
