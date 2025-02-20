c949cfdf7a70d7119d10372c7bfe7863938d3e00
Halil-ozcan <halil_ozcann@outlook.com>
2025-02-20 21:37:26 +0300

    HomeWork17 Ödevi Yapıldı.

---
+++ 01-C# ile Temel Proglamlama/Hafta-01/HomeWork_18_02_2025/HomeWork17/Program.cs
@@ -0,0 +1,9 @@
+﻿byte sayi1 = 42;
+short sayi2  = 42;
+int sayi3 = 42;
+long sayi4 = 42;
+
+Console.WriteLine("Byte'ın Bellekte Kapladığı alan : " + sizeof(byte) + "Byte");
+Console.WriteLine("Short'ın Bellekte Kapladığı alan : " + sizeof(short )+ "Byte");
+Console.WriteLine("Int'ın Bellekte Kapladığı alan : " + sizeof(int)+ "Byte");
+Console.WriteLine("Long'ın Bellekte Kapladığı alan : " + sizeof(long)+ "Byte");
