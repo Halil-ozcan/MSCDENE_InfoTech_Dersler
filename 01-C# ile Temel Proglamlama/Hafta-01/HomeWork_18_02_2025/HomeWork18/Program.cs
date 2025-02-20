e0abc2e57d2212d279d006f3b2caecb84b60d9d0
Halil-ozcan <halil_ozcann@outlook.com>
2025-02-20 21:41:52 +0300

    HomeWork18 Ödevi Yapıldı.

---
+++ 01-C# ile Temel Proglamlama/Hafta-01/HomeWork_18_02_2025/HomeWork18/Program.cs
@@ -0,0 +1,5 @@
+﻿string cumle = "Merhaba Ben Halil Özcan 23 Yaşındayım";
+
+int karakterSayisi = cumle.Length;
+
+Console.WriteLine("Karakter Sayisi : " + karakterSayisi + " Tane Karakter Vardır.");
