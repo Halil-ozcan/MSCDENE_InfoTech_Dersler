c8648146d7a7a0d6940cb6280298fa6deb96be51
Halil-ozcan <halil_ozcann@outlook.com>
2025-02-20 21:09:00 +0300

    HomeWork12 Ödevi Yapıldı.

---
+++ 01-C# ile Temel Proglamlama/Hafta-01/HomeWork_18_02_2025/HomeWork12/Program.cs
@@ -0,0 +1,8 @@
+﻿float sayi = 1.23f;
+
+double donusum =(double)sayi;
+
+Console.WriteLine($"Float değeri: {sayi}");
+Console.WriteLine($"Double değeri: {donusum}");
+
+// Double değişkenin hassasiyeti float değişkeninden fazla olduğu için floatın bellekteki gerçek değeri daha belirgin hale gelir ve bu yüzden çıktı olarak double da basamak değerleri görünür.
