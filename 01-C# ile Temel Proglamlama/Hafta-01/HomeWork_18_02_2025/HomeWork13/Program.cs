7a6d141fd4f5e48bab1e59f684e5d6a576825875
Halil-ozcan <halil_ozcann@outlook.com>
2025-02-20 21:16:50 +0300

    HomeWork13 Ödevi Yapıldı.

---
+++ 01-C# ile Temel Proglamlama/Hafta-01/HomeWork_18_02_2025/HomeWork13/Program.cs
@@ -0,0 +1,9 @@
+﻿long sayi = 500000000000;
+
+int donusum = (int) sayi;
+
+
+Console.WriteLine($"Long değeri: {sayi}");
+Console.WriteLine($"Int değeri: {donusum}");
+
+// Long değişkeni 64bit veri tutuyor. Int değişkeni ise 32bite kadar yer tutabiliyor. Int dönüşümü yapıldığında veri kaybına neden oluyor. Bu yüzden int çıktısı int değişkenin 32 bitine kadar olan değeri alır(1783793664) gerisi veri kaybına neden olur.
