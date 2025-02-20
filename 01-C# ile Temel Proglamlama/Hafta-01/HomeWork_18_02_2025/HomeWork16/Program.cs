09af774b4338dd3c1dd1f31ddf0cb1a52137677f
Halil-ozcan <halil_ozcann@outlook.com>
2025-02-20 21:30:32 +0300

    HomeWork16 Ödevi Yapıldı

---
+++ 01-C# ile Temel Proglamlama/Hafta-01/HomeWork_18_02_2025/HomeWork16/Program.cs
@@ -0,0 +1,9 @@
+﻿double pi = 3.14159;
+
+float donusum = (float)pi;
+
+
+Console.WriteLine($"Double değeri: {pi}");
+Console.WriteLine($"Float değeri: {donusum}");
+
+// Double 64bitlik bir ondalıklı sayı türü iken Float 32bitlik bir ondalıklı sayı türüdür. Bu Yüzden Double değişkenini hassasiyet değeri Float'a göre çok yüksektir. Doubledan Float'a tip dönüşümü yapıldığında hassaiyet bozulmaz ikisinde de aynı çıktıyı verir.
