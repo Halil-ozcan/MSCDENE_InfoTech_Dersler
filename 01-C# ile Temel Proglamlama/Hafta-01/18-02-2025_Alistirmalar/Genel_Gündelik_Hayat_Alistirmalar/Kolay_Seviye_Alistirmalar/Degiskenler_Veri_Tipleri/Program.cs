70095b13b407b09ed5008c22888746f17f77db38
Halil-ozcan <halil_ozcann@outlook.com>
2025-02-24 17:12:05 +0300

    Degiskenler Veri Tipleri Kolay Seviye Alıstırmalar Yapıldı

---
+++ 01-C# ile Temel Proglamlama/Hafta-01/18-02-2025_Alistirmalar/Genel_Gündelik_Hayat_Alistirmalar/Kolay_Seviye_Alistirmalar/Degiskenler_Veri_Tipleri/Program.cs
@@ -0,0 +1,43 @@
+﻿#region AdYasBilgisi
+    
+// KOLAY SEVİYE AD YAŞ BİLGİSİ
+
+Console.Write("Merhaba Adinizi Giriniz: ");
+string FullName = Console.ReadLine()!;
+
+Console.Write("Merhaba Yasinizi Giriniz: ");
+int age = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Merhaba {FullName}, sen {age} yasindasin.");
+
+#endregion
+
+#region DildortgenAlanveCevreBulma
+
+    //KOLAY SEVİYE DildortgenAlanveCevreBulma
+
+    Console.Write("Kisa Kenar Uzunluğu Giriniz: ");
+    double kisaKenar = Convert.ToDouble(Console.ReadLine());  
+
+    Console.Write("Uzun Kenar Uzunluğu Giriniz: ");
+    double uzunKenar = Convert.ToDouble(Console.ReadLine());
+
+    double alan = kisaKenar * uzunKenar;
+    double cevre = (kisaKenar + uzunKenar) * 2;
+
+    Console.WriteLine($"Girilen dikdörtgenin alani: {alan}, ve çevresi: {cevre}");
+
+    
+#endregion
+
+#region Dereceyi Fahrenheit'a Cevirme
+
+    // KOLAY SEVİYE - Dereceyi Fahrenheit'a Cevirme,
+
+    Console.Write("Lütfen derce olarak bir sicaklik değeri giriniz: ");
+    double sicaklik = Convert.ToDouble(Console.ReadLine());
+
+    double fahrenheit = sicaklik *1.8 + 32;
+    
+    Console.WriteLine($"Santigrate Derece Cinsinden Girilen {sicaklik} C'nin,  Fahreneit karsılığı : {fahrenheit}");
+#endregion
