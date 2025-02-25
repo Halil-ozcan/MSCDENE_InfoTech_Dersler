1246af9bf5d624618127622fc4499cd92471fdf7
Halil-ozcan <halil_ozcann@outlook.com>
2025-02-25 17:05:38 +0300

    Degiskenler Veri Tipleri Zor Seviye Alıstırmalar Yapıldı

--- 01-C# ile Temel Proglamlama/Hafta-01/18-02-2025_Alistirmalar/Genel_Gündelik_Hayat_Alistirmalar/Zor_Seviye_Alistirmalar/Degiskenler_Veri_Tipleri/Program.cs
+++ 01-C# ile Temel Proglamlama/Hafta-01/18-02-2025_Alistirmalar/Genel_Gündelik_Hayat_Alistirmalar/Zor_Seviye_Alistirmalar/Degiskenler_Veri_Tipleri/Program.cs
@@ -16,6 +16,33 @@
 
 #endregion
 
-#region Name
+#region ATM Simülasyonu
+    // ZOR SEVİYE - ATM Simülasyonu
+    int bakiye = 5000;
+
+    Console.WriteLine("Seçim Yapınız(1/2): ");
+    int secim = int.Parse(Console.ReadLine()!);
+
+    switch(secim)
+    {
+        case 1:
+        
+            Console.WriteLine("Lütfen Yatıracağınız parayı giriniz: ");
+            int paraYatirma = int.Parse(Console.ReadLine()!);
+            bakiye += paraYatirma;
+            Console.WriteLine($"Güncel bakiyeniz: {bakiye}");
+            break;
+        case 2:
+            Console.WriteLine("Lütfen Çekeceğiniz parayı giriniz: ");
+            int paraCekme = int.Parse(Console.ReadLine()!);
+            bakiye -= paraCekme;
+            Console.WriteLine($"Güncel Bakiyeniz: {bakiye}");
+            break;
+
+        default:
+            Console.WriteLine("Hatalı seçim yaptınız!");
+            break;
     
+
+    } 
 #endregion
