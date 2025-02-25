0ad6d5be87c1722f90ee34836507e63a69c58011
Halil-ozcan <halil_ozcann@outlook.com>
2025-02-25 10:08:03 +0300

    Devamı yapılacak

---
+++ 01-C# ile Temel Proglamlama/Hafta-01/18-02-2025_Alistirmalar/Genel_Gündelik_Hayat_Alistirmalar/Zor_Seviye_Alistirmalar/Degiskenler_Veri_Tipleri/Program.cs
@@ -0,0 +1,21 @@
+﻿#region Manav Kasa Programı
+    // ZOR SEVİYE - Manav Kasa Programı 
+
+    // Console.Write("Lütfen alacağınız meyvenin adını giriniz :");
+    // string meyveAdi = Console.ReadLine()!;
+
+    // Console.Write("Lütfen alacağınız meyvenin kg giriniz :");
+    // int kg = int.Parse(Console.ReadLine()!);
+
+    // Console.Write("Lütfen alacağınız meyvenin kg Fiyatini giriniz :");
+    // int fiyat = int.Parse(Console.ReadLine()!);
+
+    // int result = kg * fiyat;
+
+    // Console.WriteLine($"{meyveAdi} {kg} kg * {fiyat} TL/kg  Toplam Tutar => {result} TL'dir.");
+
+#endregion
+
+#region Name
+    
+#endregion
