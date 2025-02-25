bbfc55c10133c5ddb1503b652b4958dceefda374
Halil-ozcan <halil_ozcann@outlook.com>
2025-02-25 17:30:18 +0300

    Tip Dönüşümü ve Metotlar Kolay Seviye Alistirmalar Yapıldı.

---
+++ 01-C# ile Temel Proglamlama/Hafta-01/18-02-2025_Alistirmalar/Genel_Gündelik_Hayat_Alistirmalar/Kolay_Seviye_Alistirmalar/Tip_Donusumu_Metotlar/Program.cs
@@ -0,0 +1,27 @@
+﻿#region YasHesaplama
+    // Console.WriteLine("Lütfen dogum yılınızı giriniz: ");
+    // string year = Console.ReadLine()!;
+    // int age = Convert.ToInt32(year);
+    // DateTime today = DateTime.Today;
+    // int newToday = today.Year;
+    // int result = newToday -  age;
+    // Console.WriteLine($"Bugünkü yasınız: {result}");
+#endregion
+
+#region İkiSayıyıToplama
+    // Console.Write("Lütfen Birinci Sayiyi Giriniz: ");
+    // string number1 = Console.ReadLine()!;
+
+    // Console.Write("Lütfen İkinci Sayiyi Giriniz: ");
+    // string number2 = Console.ReadLine()!;
+
+    // int toplam = int.Parse(number1) + int.Parse(number2);
+    // Console.WriteLine($"{number1} + {number2} = {toplam}");
+#endregion
+
+#region MetniBüyük/KüçükHarfeÇevirme
+    Console.WriteLine("Lütfen Bir Cümle Yazınız: ");
+    string cumle = Console.ReadLine()!.ToUpper();
+    Console.WriteLine(cumle);
+    Console.WriteLine(cumle.ToLower());
+#endregion
