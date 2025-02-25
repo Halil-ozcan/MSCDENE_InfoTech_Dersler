9091aace600a5c5208bd2c64b61e998132a64feb
Halil-ozcan <halil_ozcann@outlook.com>
2025-02-25 18:06:47 +0300

    Tip Dönüşümü ve Metotlar Orta Seviye Alistirmalar Yapıldı.

---
+++ 01-C# ile Temel Proglamlama/Hafta-01/18-02-2025_Alistirmalar/Genel_Gündelik_Hayat_Alistirmalar/Orta_Seviye_Alistirmalar/Tip_Donusumu_Metotlar/Program.cs
@@ -0,0 +1,22 @@
+﻿#region KelimeSayacı
+    // Console.Write("Lütfen Bir Cümle Yazınız: ");
+    // string cumle = Console.ReadLine()!;
+    // int kelimeSayisi = cumle.Split(" ").Length;
+    // Console.WriteLine($"Kelime Sayisi: {kelimeSayisi}");
+#endregion
+
+#region TelefonNumarasıFormatlama
+    Console.Write("Lütfen 10 haneli bir telefon numarası giriniz: ");
+    string telefonNumarasi = Console.ReadLine()!;
+
+        string alanKodu = telefonNumarasi.Substring(0, 3); 
+        string ilkBolum = telefonNumarasi.Substring(3, 3); 
+        string ikinciBolum = telefonNumarasi.Substring(6, 2); 
+        string ucuncuBolum = telefonNumarasi.Substring(8, 2);
+
+        string formatlanmisNumara = $"({alanKodu}) {ilkBolum}-{ikinciBolum}-{ucuncuBolum}";
+
+        
+        Console.WriteLine($"Formatlanmış telefon numarası: {formatlanmisNumara}");
+#endregion
+
