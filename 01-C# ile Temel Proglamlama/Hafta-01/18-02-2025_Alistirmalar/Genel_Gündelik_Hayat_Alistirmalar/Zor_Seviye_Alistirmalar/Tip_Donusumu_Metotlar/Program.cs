0777fcd08de949b0f23ec6c2f127806dc738a732
Halil-ozcan <halil_ozcann@outlook.com>
2025-02-25 18:23:32 +0300

    Tip Dönüşümü ve Metotlar Zor Seviye Alistirmalar Yapıldı.

---
+++ 01-C# ile Temel Proglamlama/Hafta-01/18-02-2025_Alistirmalar/Genel_Gündelik_Hayat_Alistirmalar/Zor_Seviye_Alistirmalar/Tip_Donusumu_Metotlar/Program.cs
@@ -0,0 +1,26 @@
+﻿#region Metinden Rakamları Çekme
+    // Console.Write("Lütfen Bir Adres Giriniz: ");
+    // string cumle = Console.ReadLine()!;
+
+    // string sadeceRakamlar = new string(cumle.Where(char.IsDigit).ToArray());
+
+    // Console.WriteLine($"Sadece Rakamlar: {sadeceRakamlar}");
+#endregion
+
+#region KullanıcıAdiOlusturucu
+    Console.WriteLine("Lütfen adınızı girin:");
+    string ad = Console.ReadLine()!;
+
+    Console.WriteLine("Lütfen soyadınızı girin:");
+    string soyad = Console.ReadLine()!;
+
+    ad = char.ToUpper(ad[0]) + ad.Substring(1).ToLower();
+    soyad = char.ToUpper(soyad[0]) + soyad.Substring(1).ToLower();
+
+    Random rnd = new Random();
+    int rastgeleSayilar = rnd.Next(100,1000);
+
+    string kullaniciAdi = ad + soyad + rastgeleSayilar.ToString();
+
+    Console.WriteLine($"Oluşturulan Kullanıcı Adı: {kullaniciAdi}");
+#endregion
