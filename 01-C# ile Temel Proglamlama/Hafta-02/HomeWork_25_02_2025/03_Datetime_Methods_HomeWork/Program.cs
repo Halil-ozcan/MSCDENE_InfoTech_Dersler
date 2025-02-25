d539b55dd5177bb1009e5b64022d87c6ad492be6
Halil-ozcan <halil_ozcann@outlook.com>
2025-02-25 22:24:57 +0300

    DateTime Method Homework Ödevi  Devam Edilecek.

---
+++ 01-C# ile Temel Proglamlama/Hafta-02/HomeWork_25_02_2025/03_Datetime_Methods_HomeWork/Program.cs
@@ -0,0 +1,44 @@
+﻿#region HomeWork01
+//Kullanıcıdan alınan bir tarihin haftanın hangi günü olduğunu bulan bir program yazın.
+// Console.Write("Lütfen bir tarih giriniz: ");
+// DateTime date = Convert.ToDateTime(Console.ReadLine());
+// Console.WriteLine(date.DayOfWeek);
+#endregion
+#region HomeWork02
+//Şu anki tarih ve saati ekrana yazdıran bir program yazın.
+    // Console.WriteLine(DateTime.Now);
+#endregion
+
+#region HomeWork03
+//Kullanıcıdan alınan bir tarihe belirli bir gün sayısı ekleyerek yeni tarihi bulan bir program yazın.
+// Console.Write("Lütfen bir tarih giriniz: ");
+// DateTime date = Convert.ToDateTime(Console.ReadLine());
+// Console.WriteLine($"Eski Tarih: {date}, ve eklenen gün ile yeni tarih: {date.AddDays(4)}");
+#endregion
+
+#region HomeWork04
+//İki tarih arasındaki gün farkını hesaplayan bir program yazın.
+// Console.WriteLine("Lütfen birinci tarihi giriniz: ");
+// DateTime date1 = Convert.ToDateTime(Console.ReadLine());
+
+// Console.WriteLine("Lütfen ikinci tarihi giriniz: ");
+// DateTime date2 = Convert.ToDateTime(Console.ReadLine());
+
+// TimeSpan fark = date1 - date2;
+// int gunFarki = Math.Abs(fark.Days); 
+// Console.WriteLine($"İki tarih arasındaki gün farkı: {gunFarki} gün");
+
+#endregion
+
+#region HomeWork05
+// //Kullanıcıdan alınan bir tarihin yılın kaçıncı günü olduğunu hesaplayan bir program yazın.
+// Console.Write("Lütfen bir tarih giriniz: ");
+// DateTime date = Convert.ToDateTime(Console.ReadLine());
+// int gun = date.DayOfYear;
+// Console.WriteLine(gun);
+#endregion
+
+#region HomeWork06
+//Kullanıcıdan alınan bir yılın artık yıl olup olmadığını kontrol eden bir program yazın.
+    
+#endregion
