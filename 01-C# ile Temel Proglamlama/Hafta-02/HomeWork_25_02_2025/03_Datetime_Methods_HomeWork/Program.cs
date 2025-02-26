bf29e64f8d8cff9cb647897c4a07df112ec93dd6
Halil-ozcan <halil_ozcann@outlook.com>
2025-02-26 21:07:41 +0300

    DateTime Method Homework Ödevi Yapıldı.

--- 01-C# ile Temel Proglamlama/Hafta-02/HomeWork_25_02_2025/03_Datetime_Methods_HomeWork/Program.cs
+++ 01-C# ile Temel Proglamlama/Hafta-02/HomeWork_25_02_2025/03_Datetime_Methods_HomeWork/Program.cs
@@ -6,7 +6,7 @@
 #endregion
 #region HomeWork02
 //Şu anki tarih ve saati ekrana yazdıran bir program yazın.
-    // Console.WriteLine(DateTime.Now);
+// Console.WriteLine(DateTime.Now);
 #endregion
 
 #region HomeWork03
@@ -40,5 +40,50 @@
 
 #region HomeWork06
 //Kullanıcıdan alınan bir yılın artık yıl olup olmadığını kontrol eden bir program yazın.
-    
+// Console.Write("Bir Yil Girin: ");
+// int yil = int.Parse(Console.ReadLine()!);
+
+// if((yil % 400 == 0) || (yil % 4 == 0 && yil % 100 !=0))
+// {
+//     Console.WriteLine($"{yil} bir artik yildir.");
+// }
+// else
+// {
+//       Console.WriteLine($"{yil} bir artik yil değildir.");
+// }
+#endregion
+
+#region HomeWork08
+//Şu anki tarihin ay adını tam olarak yazdıran bir program yazın
+
+// DateTime bugun = DateTime.Now;
+// string ayAdi = bugun.ToString("MMMM");
+// Console.WriteLine($"Bugünün Ayi: {ayAdi}");
+#endregion
+
+#region HomeWork09
+//Kullanıcıdan alınan bir tarihin, başka bir kullanıcıdan alınan tarihten önce mi, sonra mı yoksa aynı mı olduğunu kontrol eden bir program yazın.
+// Console.Write("İlk tarihi girin: ");
+//  DateTime tarih1 = Convert.ToDateTime(Console.ReadLine());
+
+// Console.Write("ikinci tarihi girin: ");
+//  DateTime tarih2 = Convert.ToDateTime(Console.ReadLine());
+
+//     if (tarih1 < tarih2)
+//     {
+//         Console.WriteLine("İlk tarih, ikinci tarihten önce.");
+//     }
+//     else if (tarih1 > tarih2)
+//     {
+//         Console.WriteLine("İlk tarih, ikinci tarihten sonra.");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Her iki tarih aynı.");
+//     }   
+#endregion
+
+#region HomeWork10
+//Kullanıcıdan alınan bir saati, 12 saat formatından 24 saat formatına (veya tam tersi) çeviren bir program yazın.
+
 #endregion
