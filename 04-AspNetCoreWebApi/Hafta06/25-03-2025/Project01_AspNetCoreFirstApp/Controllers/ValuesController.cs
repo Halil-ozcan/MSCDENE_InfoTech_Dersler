39a4e7495faf8329352b53e8be2b20ce9f1ec43c
Halil-ozcan <halil_ozcann@outlook.com>
2025-03-25 20:08:04 +0300

    25-03-2025 Salı Dersini Bitirdik.

---
+++ 04-AspNetCoreWebApi/Hafta06/25-03-2025/Project01_AspNetCoreFirstApp/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project01_AspNetCoreFirstApp.Controllers
+{
+    //localhost:5200/api/values
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public IActionResult Hello()
+        {
+            return Ok("Merhaba Dünya");
+        }
+    }
+}
