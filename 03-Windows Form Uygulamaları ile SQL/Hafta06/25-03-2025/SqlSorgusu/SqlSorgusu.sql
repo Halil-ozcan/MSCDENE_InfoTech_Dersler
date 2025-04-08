227f9d9a0ffaf24efe52ee56cafd98137437330c
Halil-ozcan <halil_ozcann@outlook.com>
2025-04-08 15:28:12 +0300

    SQL Sorgu Güncelleme

--- 03-Windows Form Uygulamaları ile SQL/Hafta06/25-03-2025/SqlSorgusu/SqlSorgusu.sql
+++ 03-Windows Form Uygulamaları ile SQL/Hafta06/25-03-2025/SqlSorgusu/SqlSorgusu.sql
@@ -5,13 +5,13 @@
 --from Categories
 
 --select 
---	p.ProductID as 'Id',
---	p.ProductName as 'Name', 
---	p.CategoryID as 'CategoryId',
---	p.UnitPrice as 'Price',
---	UnitsInStock as 'Stock',
---	c.CategoryName as 'Category'
---from Products p  join Categories c on p.CategoryID = c.CategoryID
+--	p.ProductID,
+--	p.ProductName, 
+--	p.CategoryID ,
+--	p.UnitPrice,
+--	UnitsInStock
+--from Products p  
+--where ProductID = 2
 
 --select 
 --	p.ProductID as 'Id',
@@ -25,24 +25,50 @@
 
 --update Products 
 --set
---	ProductName ='',
---	CategoryID=0,
---	UnitPrice =0,
---	UnitsInStock=50
---where ProductID = 1
+--	ProductName ='@p1',
+--	CategoryID=@p2,
+--	UnitPrice =@p3,
+--	UnitsInStock=@p4
+--where ProductID = @p0
 
---insert into Products(ProductName, UnitPrice, UnitsInStock, CategoryID)
+--insert into Products(ProductName, CategoryID, UnitPrice, UnitsInStock)
 --values
---	('',0,0,1)
+--	(@p1,@p2,@p3,@p4)
+--select scope_identity()
 
---delete Products where ProductID=81
-Select
-    p.ProductID as 'Id',
-    p.ProductName as 'Name', 
-    p.CategoryID as 'CategoryId',
-    p.UnitPrice as 'Price',
-    UnitsInStock as 'Stock',
-    c.CategoryName as 'Category'
-from Products p  join Categories c on p.CategoryID = c.CategoryID
-where p.ProductName like 'c%'
-order by p.ProductId desc
+--delete Products where ProductID=@p1
+--Select
+--    p.ProductID as 'Id',
+--    p.ProductName as 'Name', 
+--    p.CategoryID as 'CategoryId',
+--    p.UnitPrice as 'Price',
+--    UnitsInStock as 'Stock',
+--    c.CategoryName as 'Category'
+--from Products p  join Categories c on p.CategoryID = c.CategoryID
+--where p.ProductName like 'c%'
+--order by p.ProductId desc
+
+--select 
+--	c.CategoryID,
+--	c.CategoryName,
+--	c.Description
+--from Categories c
+
+--select 
+--	c.CategoryID,
+--	c.CategoryName,
+--	c.Description
+--from Categories c
+--where c.CategoryID = @c1
+
+--insert into Categories(CategoryName,Description)
+--values
+--	(@p1,@p2)
+
+--update Categories
+--set
+--	CategoryName = @p1,
+--	Description = @p2
+--where CategoryID = @p0
+
+delete Categories where CategoryID=@c0
