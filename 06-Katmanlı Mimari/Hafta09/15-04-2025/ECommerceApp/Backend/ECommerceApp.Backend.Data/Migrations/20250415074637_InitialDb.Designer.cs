181aa0241b6a7041a5b667ba6d465c7149dabad2
Halil-ozcan <halil_ozcann@outlook.com>
2025-04-15 20:15:55 +0300

    15-04-2025 Salı Dersini Bitirdik.

--- 06-Katmanlı Mimari/Hafta09/15-04-2025/ECommerceApp/Backend/ECommerceApp.Backend.Data/Migrations/20250415074637_InitialDb.Designer.cs
+++ 06-Katmanlı Mimari/Hafta09/15-04-2025/ECommerceApp/Backend/ECommerceApp.Backend.Data/Migrations/20250415074637_InitialDb.Designer.cs
@@ -4,6 +4,7 @@ using ECommerceApp.Backend.Data.Concrete.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ECommerceApp.Backend.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250415074637_InitialDb")]
+    partial class InitialDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
